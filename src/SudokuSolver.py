fcdb24623ac1bbae11fc05f99236e3996bb0fb63
Michael Stal <michael.stal@gmail.com>
2022-10-24 09:50:41 +0200

    Added strategy class for pointing pairs and triples strategy

--- src/SudokuSolver.py
+++ src/SudokuSolver.py
@@ -133,6 +133,8 @@ class SudokuSolver:
         self.attachInfluenceStrategy(strategy)
         strategy = self.HiddenTriplesStrategy(self)
         self.attachInfluenceStrategy(strategy)
+        strategy = PointingPairsAndTriplesStrategy(self)
+        self.attachInfluenceStrategy(strategy)
         
     # deletes and reinitializes self._data
     # Note: registered strategies are left untouched
@@ -1750,6 +1752,88 @@ class SudokuSolver:
             return True
         else: 
             return False
+            
+"""
+The class PointingPairsAndTriplesStrategy implements an 
+Influence Strategy. If only 2 (or 3) cells in a quadrant 
+have the candidate c and f all these cells are located
+in the same row (or column), then c cannot be a candidate
+in any remaining cells of this row (or column).
+In contrast to other strategies this stratgey is 
+provided as an external class instead of an internal
+class of SudokuSolver.
+
+"""
+class PointingPairsAndTriplesStrategy(SudokuSolver.InfluenceStrategy):
+    def __init__(self, board):
+        self.board = board
+        
+    def applyStrategy(self):
+        self.applyStrategyToQuadrants()
+        
+    def handlePointingPairsAndTriples(self, cells, num):
+        assert len(cells) == DIM, "Error: array must have " + str(DIM) + " cells"
+        cand = []
+        occurrences = []
+        # go through all cells in the array
+        for idx in range(0, DIM):
+            # if n is part of the cell
+            i,j = cells[idx]
+            cand = self.board.getCandidates(i,j)
+            if num in cand:
+                # append the cell to occurrences
+                occurrences.append(cells[idx])
+        # if the number does not appear in  2 or 3 cells
+        count = len(occurrences)
+        if not count in range(2, 3+1):
+            return
+        else:
+            inSameRow = True 
+            inSameCol = True
+                
+            if count >= 2:
+                cell1 = occurrences[0]
+                cell2 = occurrences[1]
+                c1i, c1j = cell1
+                c2i, c2j = cell2
+                inSameRow = inSameRow and (c1i == c2i)
+                inSameCol = inSameCol and (c1j == c2j)
+                    
+            if count == 3:
+                cell3 = occurrences[2]
+                c3i, c3j = cell3
+                inSameRow = inSameRow and (c2i == c3i)
+                inSameCol = inSameCol and (c2j == c3j)
+            
+            (d1,d2,r,c) = self.board.inverseMapQuadrant(c1i, c1j)
+            if inSameRow:
+                low  = (d2-1)*dim + 1
+                high = (d2-1)*dim + 3
+                row = c1i
+                for c in range(1, DIM+1):
+                    if c >= low or c <= high: continue
+                    self.board.addInfluencer(num, row, c)
+            elif inSameCol:
+                low  = (d1-1)*dim + 1
+                high = (d1-1)*dim + 3
+                col = c1j
+                for r in range(1, DIM+1):
+                    if r >= low or r <= high: continue
+                    self.board.addInfluencer(num, r, col)
+                            
+                        
+    def applyStrategyToQuadrants(self):
+        # for all quadrants (d1,d2):
+        for d1 in range(1, dim+1):
+            for d2 in range(1, dim+1):
+                array = []
+                for r in range(1, dim+1):
+                    for c in range(1, dim+1):
+                        # and append the cells to array
+                        array.append(((d1-1) * dim + r, (d2-1) * dim + c))
+                # iterate through all possible numbers
+                for n in range(1, DIM+1):
+                    self.handlePointingPairsAndTriples(array, n)
 
 """           
 ######################################################################           
@@ -2155,12 +2239,12 @@ if __name__ == "main":
     
     demo(mode = 2)
 
-"""
+
 else:
-    demo(mode = 1, withCheating = True)
+    demo(mode = 1, withCheating = False)
     shell = SudokuShell()
     shell.run(withCheating = False)
-"""
+
 
 
 
